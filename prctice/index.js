38c81b523d719539a166ee2f19de549a4ae67ef6
Raushan Pandey <raushanpandeyyy@gmail.com>
2025-08-12 01:43:12 +0530

    Delete prctice directory

--- prctice/index.js
+++
@@ -1,9 +0,0 @@
-let buttom=document.querySelector("button");
-buttom.innerText="hhh";
- buttom.remove();
-
-let newbtn=document.createElement("button");
-newbtn.innerText="click me!";
-newbtn.style.backgroundColor="red";
-newbtn.style.color="white";
-document.querySelector("body").prepend(newbtn);
